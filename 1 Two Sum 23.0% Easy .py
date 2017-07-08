c9793909c5a41b1280de9947ee797fad1d7f2a58
ywCN <wangyouhao90@gmail.com>
2017-07-08 19:05:23 -0400

    Add files via upload

---
+++ 1 Two Sum 23.0% Easy .py
@@ -0,0 +1,18 @@
+'''
+Strategy: loop through the list by using enumerate to access both element and element index, and initialize a dictionary as a repository of data.
+for each element in the list, the program will check if (target - current element) is in the dictionary. 
+if it is in, return both indexes. If not, add the element as the key and its index as value in the dictionary.
+'''
+def twoSum(nums, target):
+    d = {}
+
+    for i, n in enumerate(nums):
+        m = target - n
+
+        if m in d:
+            return [d[m], i]
+        else:
+            d[n] = i
+
+
+print(twoSum([2, 7, 11, 15], 9))
