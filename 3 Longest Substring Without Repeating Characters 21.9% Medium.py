218352057c0f1c4845001b99c1715ecb85595eec
Youhao Wang <wangyouhao90@gmail.com>
2017-07-09 13:35:33 -0400

    finished coding

--- 3 Longest Substring Without Repeating Characters 21.9% Medium.py
+++ 3 Longest Substring Without Repeating Characters 21.9% Medium.py
@@ -5,3 +5,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+		used_char = {} # key: char in s		value: index of char in s
+		max_length = start = 0
+		
+		for i, c in enumerate(s):
+			if c in used_char and start <= used_char[c]:
+				start = used_char[c] + 1
+			else:
+				max_length = max(max_length, i - start + 1)
+			
+			used_char[c] = i
+			
+		return max_length
