073edee6883377f6790f53511b94b6dc0bff8a1f
Youhao Wang <wangyouhao90@gmail.com>
2017-07-09 19:14:17 -0400

    added comments and fixed format a little

--- 5 Longest Palindromic Substring 23.0% Medium .py
+++ 5 Longest Palindromic Substring 23.0% Medium .py
@@ -1,5 +1,11 @@
 # O(n^2)
 
+# This is a slow solution because it does some duplicated work in the string. However, it is easy to understand.
+
+# This program will loop through the string from left to right,
+# trying to check if an element can expand to a larger palindrome substring,
+# and returns the longest substring
+
 def longestPalindrome(s):
     """
     :type s: str
@@ -8,14 +14,17 @@ def longestPalindrome(s):
 
     res = ""
     for i in range(len(s)):
+
         # odd case, like "aba"
         tmp = helper(s, i, i)
         if len(tmp) > len(res):
             res = tmp
+
         # even case, like "abba"
         tmp = helper(s, i, i + 1)
         if len(tmp) > len(res):
             res = tmp
+
     return res
 
 
@@ -24,10 +33,11 @@ def longestPalindrome(s):
 
 def helper(s, l, r):
     while l >= 0 and r < len(s) and s[l] == s[r]:
-        l -= 1
-        r += 1
+        l -= 1  # expand to left
+        r += 1  # expand to right
     return s[l + 1:r]
 
-
+'''
 x = longestPalindrome('FGGFFadfsssfbd')
 print(x)
+'''
