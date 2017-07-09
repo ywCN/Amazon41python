756324000c1f81205560edce5102aaa3402d07f7
Youhao Wang <wangyouhao90@gmail.com>
2017-07-08 23:43:57 -0400

    added comments and thoughts

--- 2 Add Two Numbers 23.0% Medium .py
+++ 2 Add Two Numbers 23.0% Medium .py
@@ -4,6 +4,11 @@
 #         self.val = x
 #         self.next = None
 
+'''
+walk through the two object while Node is not None.
+get the val fields of two objects and add two fields and carry, then use divmod to return a tuple of times and remainder.
+'''
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -11,24 +16,25 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-		carry = 0
-		root = n = ListNode(0)
+		carry = 0	# carry variable stores quotient（商）of divmod 
+		root = n = ListNode(0)	# create a new linked list object to store the result
 		
 		while 11 or 12 or carry:
-			v1 = v2 = 0
+			v1 = v2 = 0	# initialize two varibales to copy val fields of objects
 		
 			if l1:
-				v1 = l1.val
-				l1 = l1.next
+				v1 = l1.val	# copy val of current node of l1
+				l1 = l1.next # walk to next object of the Slinked List l1
 				
 			if l2:
-				v2 = l2.val
-				l2 = l2.next
+				v2 = l2.val # copy val of current node of l2
+				l2 = l2.next # walk to next object of the Slinked List l2
 			
-			carry, val = divmod(v1 + v2 + carry, 10)
-			n.next = ListNode(val)
-			n = n.next
+			carry, val = divmod(v1 + v2 + carry, 10) 
+			# divmod is a combination of divide and mod, it returns a tuple
+			n.next = ListNode(val) # create a new ListNode object in the new linked list, set the val field to calculated val
+			n = n.next # n is now referencing to the new object in the new linked list
 			
-			return root.next
+			return root.next # after looping, return the new ListNode
 			
 		
