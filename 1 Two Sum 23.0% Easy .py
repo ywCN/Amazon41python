ce2f2eb5c4699cb57a1aefae434aa2e7d78acc65
Youhao Wang <wangyouhao90@gmail.com>
2017-07-09 23:45:03 -0400

    format fix

--- 1 Two Sum 23.0% Easy .py
+++ 1 Two Sum 23.0% Easy .py
@@ -1,11 +1,16 @@
-'''
-Strategy: loop through the list by using enumerate to access both element and element index, and initialize a dictionary as a repository of data.
-for each element in the list, the program will check if (target - current element) is in the dictionary. 
-if it is in, return both indexes. If not, add the element as the key and its index as value in the dictionary.
-'''
+# Strategy: loop through the list by using enumerate to access both element
+# and element index, and initialize a dictionary as a repository of data.
 
+# for each element in the list, the program will check
+# if (target - current element) is in the dictionary.
+
+# if it is in, return both indexes. If not,
+# add the element as the key and its index as value in the dictionary.
+
+#
 # comment to test commit and push
 def twoSum(nums, target):
+
     d = {}
 
     for i, n in enumerate(nums):
