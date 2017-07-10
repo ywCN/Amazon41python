467ca638156925dc3b6b756d6898eca9d448770a
Youhao Wang <wangyouhao90@gmail.com>
2017-07-09 23:26:33 -0400

    finished coding and add a little comments

--- 8 String to Integer (atoi) 13.5% Easy .py
+++ 8 String to Integer (atoi) 13.5% Easy .py
@@ -13,3 +13,44 @@
 # it contains only whitespace characters, no conversion is performed.
 
 
+def myAtoi(s):
+    """
+    :type str: str
+    :rtype: int
+    """
+
+    s = s.strip()
+
+    if len(s) == 0:
+        return 0
+
+    tmp = '0'
+    result = 0
+    i = 0  # stores the current index of the string s
+
+    if s[0] in '-+':
+        tmp = s[0]
+        i = 1
+
+    MAX_INT = 2147483647
+    MIN_INT = -2147483648
+
+    for i in range(i, len(s)):
+
+        if s[i].isdigit():
+            tmp += s[i]
+        else:
+            break
+
+        if len(tmp) > len(MAX_INT) - 1:
+            result = int(tmp)
+
+        if result > MAX_INT:
+            return MAX_INT
+
+        elif result < MIN_INT:
+            return MIN_INT
+
+        else:
+            return result
+
