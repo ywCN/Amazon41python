8391645eeed7e5405044a71b697467fd6f625047
Youhao Wang <wangyouhao90@gmail.com>
2017-07-10 15:45:40 -0400

    finished coding

--- 15 3Sum 18.8% Medium .py
+++ 15 3Sum 18.8% Medium .py
@@ -1,1 +1,35 @@
 
+def threeSUm(nums):
+	"""
+	type nums: List[int]
+	rtype: List[List[int]]
+	"""
+	
+	nums = sorted(nums)
+	result = []
+	
+	for i in range(len(nums) - 2):  
+	# -2 because the result must be a 3 elements list
+		if i > 0 and nums[i] == nums[i - 1]: # because the answers should be unique
+			continue
+		
+		l, r = i + 1, len(nums) - 1
+		
+		while r > l:
+			s = nums[i] + nums[l] + nums[r]
+			if s < 0:
+				l += 1
+			elif s > 0:
+				r -= 1
+			else:
+				result.append([nums[i], nums[l], nums[r]])
+				while l < r and nums[l] == nums[l + 1]
+					l += 1
+				while l < r and nums[r] == nums[r - 1]
+					r -= 1
+				l += 1
+				r -= 1
+				
+		return result
+			
+		
