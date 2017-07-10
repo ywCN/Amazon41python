103b533c98324162cb57dcbe4b8ed7aa2f2fb14b
Youhao Wang <wangyouhao90@gmail.com>
2017-07-10 17:54:51 -0400

    finished coding, commenting, dubugging

--- 17 Letter Combinations of a Phone Number 28.6% Medium .py
+++ 17 Letter Combinations of a Phone Number 28.6% Medium .py
@@ -1,1 +1,16 @@
+#  https://discuss.leetcode.com/topic/11783/one-line-python-solution/22
+
+def letterCombinations(digits):
+    if digits == '':  # special case
+        return []
+    letter_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+    res = ['']
+    for digit in digits:
+        tmp = []  # store result for this digit in this loop
+        for y in res:  # loop each element in current result
+            for x in letter_map[digit]:  # loop each element for the letters of current number
+                tmp.append(y + x)
+        res = tmp
+
+    return res
 
