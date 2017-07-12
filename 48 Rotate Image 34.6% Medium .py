48cf82ea196ed77d68742c0f7eb5509fdd39e865
Youhao Wang <wangyouhao90@gmail.com>
2017-07-11 21:20:20 -0400

    finished coding and commenting

--- 48 Rotate Image 34.6% Medium .py
+++ 48 Rotate Image 34.6% Medium .py
@@ -1,1 +1,28 @@
+# https://discuss.leetcode.com/topic/6796/a-common-method-to-rotate-the-image/31
 
+"""
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+"""
+
+
+def rotate(matrix):
+    matrix.reverse()
+    # reverse up to down
+    #  if [::-1], not in-place
+
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]  # swap the symmetry
+
+
+"""
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+"""
